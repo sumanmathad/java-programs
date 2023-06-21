74834593dace1c0761d364886100f09c239d4613
suman_mathad <suman.mathad@gmail.com>
2023-06-21 20:55:33 +0530

    Solution LC 2235 Add two integers

---
+++ src/LeetCode/Solutions/_2235_AddTwoIntegers.java
@@ -0,0 +1,14 @@
+package LeetCode.Solutions;
+
+public class _2235_AddTwoIntegers {
+/*
+Given two integers num1 and num2, return the sum of the two integers.
+ */
+    public static void main(String[] args) {
+        int num1=-13;
+        int num2=-15;
+        System.out.println("Input is : "+num1+" & "+num2);
+        System.out.println("output is : ");
+        System.out.println(num2+num1);
+    }
+}
