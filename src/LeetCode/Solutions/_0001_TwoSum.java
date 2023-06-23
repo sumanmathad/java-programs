af3351566fa61cb64f5af3960f88c0bf30e54dd3
suman_mathad <suman.mathad@gmail.com>
2023-06-23 23:34:55 +0530

    Solution LC 0001 Two Sum

---
+++ src/LeetCode/Solutions/_0001_TwoSum.java
@@ -0,0 +1,23 @@
+package LeetCode.Solutions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class _0001_TwoSum {
+    public static void main(String[] args) {
+        int[] nums={3,3}; int target=6;
+
+    Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+        map.put(nums[i], i);
+    }
+        for (int i = 0; i < nums.length; i++) {
+        int complement = target - nums[i];
+        if (map.containsKey(complement) && map.get(complement) != i) {
+            System.out.println( new int[] { i, map.get(complement) });
+        }
+    }
+    // In case there is no solution, we'll just return null
+        System.out.println( "No values exist");
+}
+}
