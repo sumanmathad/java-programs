93d5adcb59fdab238f176d32f06893a7294dcdb9
suman_mathad <suman.mathad@gmail.com>
2023-08-03 07:51:12 +0530

    refactor LC:0007 Reverse Integer

--- src/LeetCode/Solutions/_0007_ReverseInteger.java
+++ src/LeetCode/Solutions/_0007_ReverseInteger.java
@@ -3,28 +3,29 @@ package LeetCode.Solutions;
 import java.time.LocalDate;
 
 public class _0007_ReverseInteger {
+/*
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+ */
+
+   public static int reverse(int x){
+       int rev=0;
+       while(x!=0){
+           int pop= x % 10;
+           x/=10;
+           if ((rev>Integer.MAX_VALUE/10)||(rev==Integer.MAX_VALUE&&pop>7))
+               return 0;
+           if ((rev<Integer.MIN_VALUE/10)||(rev==Integer.MIN_VALUE && pop<-8))
+               return 0;
+           rev=(rev*10)+pop;
+       }
+       return rev;
+   }
+
+
     public static void main(String[] args) {
-        int input=-1989023449;
-        System.out.println("The input Integer is:"+input);
-
-        int rev=0;
-        while(input!=0){
-            int pop = input % 10;
-            input /= 10;
-            if (rev > Integer.MAX_VALUE/10 || (rev == Integer.MAX_VALUE / 10 && pop > 7))
-            {
-                rev = 0;
-                break;
-            }
-            if (rev < Integer.MIN_VALUE/10 || (rev == Integer.MIN_VALUE / 10 && pop < -8))
-            {
-                rev = 0;
-                break;
-            }
-            rev = rev * 10 + pop;
-        }
-        System.out.println(rev);
-
-
+        System.out.println(reverse(89023449));
+        System.out.println(reverse(-456));
     }
 }
