6d882b44e319d70c724e03da9656ad346828caf2
suman_mathad <suman.mathad@gmail.com>
2023-08-05 14:54:42 +0530

    updates to Algorithms

---
+++ src/Algorithms/sort/BubbleSort.java
@@ -0,0 +1,35 @@
+package Algorithms.sort;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+    public static String bubbleSort(int[] nums){
+        for(int lastUnsortedindex= nums.length-1;lastUnsortedindex>=0;lastUnsortedindex--){
+            for(int i=0;i<lastUnsortedindex;i++){
+
+                if(nums[i]>nums[i+1]){
+                    swap(nums,i,i+1);
+                }
+            }
+
+        }
+        return Arrays.toString(nums);
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        if(i==j){
+            return;
+        }
+        int temp = nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(bubbleSort(new int[]{2,5,34,6,0,1,-6,9}));
+    }
+
+
+
+}
