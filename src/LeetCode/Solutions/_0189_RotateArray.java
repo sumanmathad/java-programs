bad19a80dabee2a6a15d9ce4de49fb3f716e6d11
suman_mathad <suman.mathad@gmail.com>
2023-08-14 21:01:25 +0530

    LC 189 solution for Rotate Array

---
+++ src/LeetCode/Solutions/_0189_RotateArray.java
@@ -0,0 +1,29 @@
+package LeetCode.Solutions;
+/*
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+ */
+import java.util.Arrays;
+
+public class _0189_RotateArray {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(rotate(new int[]{1,2,3,4,5,6,7},3)));
+    }
+    public static int[] rotate(int[] nums, int k){
+        k%= nums.length;
+        reverse(nums,0, nums.length-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k, nums.length-1);
+        return nums;
+    }
+    public static void reverse(int[]nums, int start, int end){
+        while( start<end) {
+            int temp = nums[start];
+            nums[start]=nums[end];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+    }
+
+}
