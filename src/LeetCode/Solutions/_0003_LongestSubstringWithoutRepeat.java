2c2c8aff6fd698323852dad26755e2b5ffb6d6c9
suman.mathad <suman.mathad@oracle.com>
2023-09-22 18:07:27 +0530

    Leetcode solution updates

--- src/LeetCode/Solutions/_0003_LongestSubstringWithoutRepeat.java
+++ src/LeetCode/Solutions/_0003_LongestSubstringWithoutRepeat.java
@@ -1,52 +1,25 @@
 package LeetCode.Solutions;
 
-import java.util.*;
-
 /*
 Given a string s, find the length of the longest substring without repeating characters.
  */
 public class _0003_LongestSubstringWithoutRepeat {
     public static void main(String[] args) {
-        System.out.println(lengthOfLongestSubstring("psdfgsdjfhgfgs"));
+        System.out.println(lengthOfLongestSubstring(" "));
     }
     public static int lengthOfLongestSubstring(String s) {
-        if(s.length()==0){
-            return 0;
-        }
-        if (s.isEmpty() && s.length()==0 ){
-            return 1;
-        }
-        Set<Character> set = new HashSet<>();
-        List<Character> list = new ArrayList<>();
-
         int maxsize=0;
-        for(char c:s.toCharArray()){
-            if(list.contains(c)){
-                if(list.size()>maxsize) {
-                    maxsize = list.size();
+        for (int i=0;i<s.length();i++){
+            StringBuilder currentSubString = new StringBuilder();
+            for(int j=i;j<s.length();j++){
+                if(currentSubString.indexOf(String.valueOf(s.charAt(j)))!=-1){
+                    break;
                 }
-                for(int i=0;i<=list.indexOf(c);i++){
-                    list.remove(i);
-                }
+                currentSubString.append(s.charAt(j));
+                maxsize =Math.max(maxsize,currentSubString.length());
             }
-            list.add(c);
 
         }
-
-
         return maxsize;
     }
-
 }
-/*
-            if(!set.add(c)){
-                if(set.size()>maxsize) {
-                    maxsize = set.size();
-                }
-                set.
-            }
-            if(c==s.charAt(s.length()-1)&&set.size()>maxsize ){
-                maxsize = set.size();
-
-            }
- */
