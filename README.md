07d1d85088272d00d28495b48429f5931f2b017a
suman.mathad <suman.mathad@oracle.com>
2023-11-16 19:45:29 +0530

    Binary search updates

--- README.md
+++ README.md
@@ -1,11 +1,25 @@
 # Java Programs and Algorithms
 This is a repository for various Java Programs and Algorithms. LeetCode is currently the major source for this repository.
 
+|    Algorithms     |                                        Algorithm with Program Solutions                                         | 
+|:-----------------:|:---------------------------------------------------------------------------------------------------------------:|  
+| Search Algorithms | [Binary Search](https://github.com/sumanmathad/java-programs/blob/main/src/Algorithms/search/BinarySearch.java) | 
+|                   |                                                                                                                 |
+|                   |                                                                                                                 |
+
+
+
+
+
 Refer below for programs details. 
 
 Note : 
 1. Multiple approaches have been recorded in the same program class file with various complexities.
 2. This is always a work in progress.
+
+
+
+
   
 
 | Problem Number (LeetCode Reference)                           | Program Name (Solution reference)           | Strategies        |
--- src/Algorithms/search/BinarySearch.java
+++ src/Algorithms/search/BinarySearch.java
@@ -3,25 +3,27 @@ package Algorithms.search;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        System.out.println(binarySearch(new int[]{2,5,8,10,16,18}, 10));
+        System.out.println(binarySearch(new int[]{2,5,8,10,16,18,19,24,27}, 100));
 
     }
 
 
     public static int binarySearch(int[]numbers,int numToFind){
-        int low=0;
-        int high=numbers.length-1;
-        while(low<=high){
-            int middlePos = (low+high)/2;
-            if(numbers[middlePos]==numToFind){
-                return middlePos;
+        int i=0;
+        int j=numbers.length-1;
+        while(i<=j){
+            int mid = (i+j)/2;
+            if(numbers[mid]==numToFind){
+                return mid;
             }
-            else if (numbers[middlePos]>numToFind){
-                high=middlePos-1;
+            else if (numbers[mid]<numToFind){
+                i=mid+1;
             }
             else
-                low=middlePos+1;
+                j=mid-1;
         }
-        return -1;
+        System.out.println("i="+i); // index in array where you can insert the searching num if you want
+        System.out.println("j="+j); //index of a num present in array which is less than the searching num
+        return -1;  // is output when searching number is not present
     }
 }
